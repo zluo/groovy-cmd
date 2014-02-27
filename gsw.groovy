d83363be1ef4e95ed076f8f08c836299d5b679de
zluo <luozhanqing@gmail.com>
2014-02-27 15:20:03 -0500

    format time

--- gsw.groovy
+++ gsw.groovy
@@ -1,16 +1,28 @@
 #!/usr/bin/env groovy
 if (args)
 {
- def address = "http://www.google.com/finance/getprices?i=1&p=1d&f=d,o,h,l,c,v&df=cpct&q=${args[0]}"
+args.each{
+ println "=================== ${it} ======================"
+ def address = "http://www.google.com/finance/getprices?i=1&p=1d&f=d,o,h,l,c,v&df=cpct&q=${it}"
  download(address)
+ }
  }
 else
 {
-println "Hello Stranger"
+println "Need Ticker"
 }
 
 def download(address)
 {
     def data = new URL(address).getText()
-//	data.eachLine{println it.grep(~/^a./)}
+    data.eachLine{it ->
+	if (it =~/^a/)
+	{
+	    def a = it.split(",")
+		
+		a[0]= new Date((Long.valueOf(a[0].substring(1))*1000)).format("yyyy-M-d hh:mm")
+		println a
+	}
+	}
 }
+
