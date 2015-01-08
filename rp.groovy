88b7d5e142c12ef0c40bbca12cab89ed462003a6
unknown <zluo@ZLUO207.silanis.com>
2015-01-08 15:52:51 -0500

    change [aws.document.engine.url] to [http://10.0.4.44/:80/axis2/services/DEService]

--- rp.groovy
+++ rp.groovy
@@ -9,6 +9,8 @@
 Properties props = new Properties()
 File propsFile = new File(args[0])
 props.load(propsFile.newDataInputStream())
+println "args[1]: ${args[1]"}
+println "args[2]: ${args[2]"}
 
 props.setProperty(args[1], args[2])
 props.store(propsFile.newWriter(), null)
